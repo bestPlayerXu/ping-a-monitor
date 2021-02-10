39efa1b60ec2275baf3263b05e401660c1980afa
bestPlayer_xu <max.mingxu@gmail.com>
2021-02-10 10:10:16 +0100

    fixed bug

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = (url, interval, ping, options) => {
 	if (!isNumber(interval)) throw 'The second argument, the `interval` has to be a number';
 	var ping = () => {
-		if (!ping.ping || !isNumber(ping.ping)) return console.warn('Nothing has been sent because the third argument need to be an object that contains a key with `ping` which has to be a number');
+		if (!ping || !isNumber(ping.ping)) return console.warn('Nothing has been sent because the third argument need to be an object that contains a key with `ping` which has to be a number');
 		try {
 			require('node-fetch')(url + (options && options.sendInQuery ? '?' + ping.ping : ''), options && options.sendInJSON ? { ping: ping.ping } : '' + ping.ping);
-		catch (e) { console.warn(e) }
+		} catch (e) { console.warn(e) }
 	}
 	if (options && options.sendAtStart) ping();
 	setInterval(ping, interval);
