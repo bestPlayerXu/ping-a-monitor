8a8ab5259d987b4fe0e62cc17785bd71fa13d257
bestPlayer_xu <max.mingxu@gmail.com>
2021-02-10 10:46:49 +0100

    Fixed bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = (url, interval, ping, options) => {
 		var pingRN = ping();
 		if (isNaN(pingRN)) return console.warn('Nothing has been sent because the third argument need to be a function that returns a number. Right now it is: ' + pingRN);
 		try {
-			require('node-fetch')(url + (options && options.sendInQuery ? '?' + pingRN : ''), options && options.sendInJSON ? { ping: pingRN } : '' + pingRN);
+			require('node-fetch')(url + (options && options.sendInQuery ? '?ping=' + pingRN : ''), options && options.sendInJSON ? { ping: pingRN } : '' + pingRN);
 		} catch (e) { console.warn(e) }
 	}
 	if (options && options.sendAtStart) pingTheMonitor();
