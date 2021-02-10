2f7add9da2b00e95fd90b893ccb33b54101cc9f5
bestPlayer_xu <max.mingxu@gmail.com>
2021-02-10 10:29:55 +0100

    ping now function instead of object

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 module.exports = (url, interval, ping, options) => {
 	if (isNaN(interval)) throw 'The second argument, the `interval` has to be a number. Instead, ' + interval + ' has been passed as an argument.';
 	var ping = () => {
-		if (!ping || isNaN(ping.ping)) return console.warn('Nothing has been sent because the third argument need to be an object that contains a key with `ping` which has to be a number. Right now it is: ' + (ping ? ping.ping : 'undefined'));
+		var pingRN = ping();
+		if (!pingRN) return console.warn('Nothing has been sent because the third argument need to be a function that returns a number. Right now it is: ' + pingRN);
 		try {
-			require('node-fetch')(url + (options && options.sendInQuery ? '?' + ping.ping : ''), options && options.sendInJSON ? { ping: ping.ping } : '' + ping.ping);
+			require('node-fetch')(url + (options && options.sendInQuery ? '?' + pingRN : ''), options && options.sendInJSON ? { ping: pingRN } : '' + pingRN);
 		} catch (e) { console.warn(e) }
 	}
 	if (options && options.sendAtStart) ping();
