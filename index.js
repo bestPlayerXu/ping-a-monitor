a44d37b712e209e85a44e0aaea91345a8fa43a76
bestPlayer_xu <max.mingxu@gmail.com>
2021-02-10 10:24:10 +0100

    fixed bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = (url, interval, ping, options) => {
-	if (isNaN(interval)) throw 'The second argument, the `interval` has to be a number';
+	if (isNaN(interval)) throw 'The second argument, the `interval` has to be a number. Instead, ' + interval + ' has been passed as an argument.';
 	var ping = () => {
-		if (!ping || isNaN(ping.ping)) return console.warn('Nothing has been sent because the third argument need to be an object that contains a key with `ping` which has to be a number');
+		if (!ping || isNaN(ping.ping)) return console.warn('Nothing has been sent because the third argument need to be an object that contains a key with `ping` which has to be a number. Right now it is: ' + (ping ? ping.ping : 'undefined'));
 		try {
 			require('node-fetch')(url + (options && options.sendInQuery ? '?' + ping.ping : ''), options && options.sendInJSON ? { ping: ping.ping } : '' + ping.ping);
 		} catch (e) { console.warn(e) }
