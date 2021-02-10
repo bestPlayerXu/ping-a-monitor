e6842c40f4c7709b78962c19d3abf52381301dcd
bestPlayer_xu <max.mingxu@gmail.com>
2021-02-10 10:36:30 +0100

    fixed dump bug

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = (url, interval, ping, options) => {
 	if (isNaN(interval)) throw 'The second argument, the `interval` has to be a number. Instead, ' + interval + ' has been passed as an argument.';
-	var ping = () => {
+	var pingTheMonitor = () => {
 		var pingRN = ping();
 		if (!pingRN) return console.warn('Nothing has been sent because the third argument need to be a function that returns a number. Right now it is: ' + pingRN);
 		try {
 			require('node-fetch')(url + (options && options.sendInQuery ? '?' + pingRN : ''), options && options.sendInJSON ? { ping: pingRN } : '' + pingRN);
 		} catch (e) { console.warn(e) }
 	}
-	if (options && options.sendAtStart) ping();
-	setInterval(ping, interval);
+	if (options && options.sendAtStart) pingTheMonitor();
+	setInterval(pingTheMonitor, interval);
 }
